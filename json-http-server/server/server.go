2fc0c3ef1938a2c9c0dbd0de86ff95cc56dfb33f
Enrique Ulises Báez Gómez Tagle <0241823@up.edu.mx>
2024-08-21 14:25:34 -0600

    refactor: update endpoints for log append and fetch

--- json-http-server/server/server.go
+++ json-http-server/server/server.go
@@ -12,9 +12,9 @@ func StartServer() {
 	// New Instance for our log struct
 	log := &log.Log{}
 
-	// Setting up HTTP handlers to produce and consume logs
-	http.HandleFunc("/produce", log.Append)
-	http.HandleFunc("/consume", log.Fetch)
+	// Setting up HTTP handlers to append and fetch logs
+	http.HandleFunc("/append", log.Append)
+	http.HandleFunc("/fetch", log.Fetch)
 
 	// Serve on port 8080
 	fmt.Println("Server started on port 8080")
